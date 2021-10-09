263ce096e329ed204251fb05717b2946d2ab5ab6
Premjit-Chowdhury <sripremjitchowdhury@gmail.com>
2021-10-09 10:08:15 +0530

    2nd commit

---
+++ app/src/main/java/com/mydeadapp/Holidays.java
@@ -0,0 +1,21 @@
+package com.mydeadapp;
+
+public class Holidays {
+    
+    
+    private final String name;
+    private final String date;
+
+    public Holidays(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
